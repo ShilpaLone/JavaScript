e52180409d7aec6a487e348f86aa5b52d6781024
ShilpaLone <shilpalone93@gmail.com>
2022-10-02 18:26:43 +0530

    SL-JavaScript

---
+++ DAY1/introduction.js
@@ -0,0 +1,67 @@
+//=============Data Types==============
+// 1. primitive
+// a. string
+// "SHILPA", "20" "BACKTICKS"
+
+// b. number
+// 10, 10, 2
+
+// c. boolean
+//true OR false
+
+// d. null
+// null
+
+// e. undefined
+// undefined
+
+// f. symbol
+// 2.Non-primitive OR Refrance type
+
+// a. array
+// [10,1,2,3,4,5];
+
+// b. object
+// const a = {
+    // key:"value",
+    // key2:10,
+    // isvalid: false,
+    // kuchbhi: [],
+    // obj: {name: "xyz", age: 20},
+//};
+// c. functions
+// function mydecision(){}
+
+// asigning data types in a variables
+
+var str ="exaltech software";
+var num = 20;
+var islogin = false;
+var dontknow = null;
+var patanahi = undefined;
+
+var names = ["a", "c", "d", "h","k"];
+var empdetails = {empname:"xyz", empage:20, empcode:"ET/1/2022/NED"};
+
+function FunctionName() {
+    return 10 + 20;
+}
+
+document.write(str);
+console.log("---->", empdetails);
+console.warn("====>", names);
+console.error("error");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
